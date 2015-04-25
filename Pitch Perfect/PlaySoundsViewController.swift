5cdd54363a4a356dd581f1efc588439e8b5b3a29
NevenRakonic <neven.rakonic@gmail.com>
2015-04-25 14:39:06 +0200

    Fast button added

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
 
-
+    var audioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
+            var fileURL = NSURL(fileURLWithPath: filePath)
+            audioPlayer = AVAudioPlayer(contentsOfURL: fileURL, error: nil)
+            audioPlayer.enableRate = true
+            audioPlayer.prepareToPlay()
+        } else {
+            println("file didn't load")
+        }
 
         // Do any additional setup after loading the view.
     }
@@ -23,6 +33,9 @@ class PlaySoundsViewController: UIViewController {
     }
     
     @IBAction func slowButton(sender: UIButton) {
+        audioPlayer.stop()
+        audioPlayer.rate = 0.5
+        audioPlayer.play()
         
     }
     
